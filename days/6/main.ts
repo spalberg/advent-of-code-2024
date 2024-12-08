96c1c7ffa03aa34a699c9ea865e3af90fa37410b
Sven Palberg <sven@palberg.de>
2024-12-08 17:28:36 +0100

    day 6 (#19)
    * day 6 part 1
    
    * day 6 part 2 wip
    
    * day 6 unoptimized
    
    * day 6 better performance
    
    * day 6 even better performance
    
    * day 6 test cov and performance
    
    * day 6 enhance test converage
    
    * day 6 enhance test converage

--- days/6/main.ts
+++ days/6/main.ts
@@ -1,11 +1,64 @@
+import { DirectedGridPosition, Grid } from "@pal/datastructures";
 import { loadInput } from "utils";
 
-export function part1(_input: Array<string>) {
-  return 0;
+export function part1(input: Array<string>) {
+  const guardPosition = parseInput(input);
+  const { path } = getGuardPositions([guardPosition]);
+  return new Set(path.map((pos) => pos.positionString)).size;
 }
 
-export function part2(_input: Array<string>) {
-  return 0;
+export function part2(input: Array<string>) {
+  const guardPosition = parseInput(input);
+  const { path } = getGuardPositions([guardPosition]);
+  const obstaclePositions = new Set<string>();
+  for (let i = 2; i < path.length; i++) {
+    const currentPos = path[i];
+    const subPath = path.slice(0, i - 1);
+    if (
+      currentPos.value === "^" ||
+      subPath.find((p) => p.isSamePosition(currentPos))
+    ) {
+      continue;
+    }
+    const { isLoop } = getGuardPositions(
+      subPath,
+      (p) => p.value === "#" || p.isSamePosition(currentPos),
+    );
+    if (isLoop) {
+      obstaclePositions.add(currentPos.positionString);
+    }
+  }
+  return obstaclePositions.size;
+}
+
+function getGuardPositions(
+  path: Array<DirectedGridPosition<string>>,
+  isObstacle = (p: DirectedGridPosition<string>) => p.value === "#",
+): { path: Array<DirectedGridPosition<string>>; isLoop: boolean } {
+  const visited = new Set(path.map((pos) => pos.toString()));
+  let guardPosition = path[path.length - 1];
+  while (true) {
+    for (
+      guardPosition of guardPosition.moveInDirectionUntil(isObstacle)
+    ) {
+      const positionAsString = guardPosition.toString();
+      if (visited.has(positionAsString)) {
+        return { path, isLoop: true };
+      }
+      path.push(guardPosition);
+      visited.add(positionAsString);
+    }
+    if (guardPosition.moveInDirection() == null) {
+      return { path, isLoop: false };
+    }
+    guardPosition = guardPosition.turnRight();
+  }
+}
+
+function parseInput(input: Array<string>): DirectedGridPosition<string> {
+  const grid = Grid.fromStrings(input);
+  const [x, y] = grid.findFirst((value) => value === "^")!;
+  return new DirectedGridPosition(grid, x, y, "up");
 }
 
 if (import.meta.main) {
--- days/6/main_test.ts
+++ days/6/main_test.ts
@@ -16,22 +16,31 @@ const input = [
   "......#...",
 ];
 
-describe.ignore("day 6 example", () => {
+// day 6 example ...
+//   part 1 ... ok (3ms)
+//   part 2 ... ok (3ms)
+// day 6 example ... ok (8ms)
+// day 6 solution ...
+//   part 1 ... ok (133ms)
+//   part 2 ... ok (54s)
+// day 6 solution ... ok (54s)
+
+describe("day 6 example", () => {
   it("part 1", () => {
     expect(part1(input)).toBe(41);
   });
 
-  it.ignore("part 2", () => {
-    expect(part2(input)).toBe(-1);
+  it("part 2", () => {
+    expect(part2(input)).toBe(6);
   });
 });
 
-describe.ignore("day 6 solution", () => {
+describe("day 6 solution", () => {
   it("part 1", async () => {
-    expect(part1(await loadInput(6))).toBe(-1);
+    expect(part1(await loadInput(6))).toBe(4826);
   });
 
   it("part 2", async () => {
-    expect(part2(await loadInput(6))).toBe(-1);
+    expect(part2(await loadInput(6))).toBe(1721);
   });
 });
